35a46159c37b60ed8b41461ca9cc46fb92cb6f5a
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-08-12 15:17:06 -0300

    feat: implemented registerUser endpoint

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -2,19 +2,19 @@ import { Request, Response } from 'express';
 import { UserType, Validation } from '../services/Validation';
 import { User } from '../models/User';
 import { UserRepository } from '../repositories/UserRepository';
-import { ZodError } from 'zod';
 import { HandleError } from '../services/HandleError';
+import { JsonWebToken } from '../services/JsonWebToken';
+import { JwtToken } from '../models/JwtToken';
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    const { name, email, password, profileImage } = req.body as UserType;
+    const { name, email, password } = req.body as UserType;
     Validation.User.parse({
       name: name,
       email: email,
       password: password,
-      profileImage: profileImage,
     });
-    const userObj = new User(name, email, password, profileImage);
+    const userObj = new User(name, email, password);
     const findEmail = await UserRepository.findFirst({
       where: { email: email },
     });
@@ -24,11 +24,40 @@ export const createUser = async (req: Request, res: Response) => {
         email: userObj.getEmail(),
         name: userObj.getName(),
         password: userObj.getPassword(),
-        profileImage: userObj.getProfileImage(),
       },
     });
-    return res.status(201).json(user);
-  } catch (error: any) {
+    const JwtPayload = new JwtToken(
+      'rrcasaque@hotmail.com',
+      new Date(new Date().getTime() + 3600 * 24).getTime(),
+      new Date().getTime(),
+      new Date().getTime(),
+      req.socket.remoteAddress as string
+    );
+    const token = JsonWebToken.generateToken(JwtPayload);
+    return res.status(201).json({ user, token });
+  } catch (error) {
+    const errors = HandleError.getErrors(error);
+    res.status(errors.status).json(errors.message);
+  }
+};
+
+export const teste = async (req: Request, res: Response) => {
+  try {
+    const JwtPayload = new JwtToken(
+      'rrcasaque@hotmail.com',
+      new Date(new Date().getTime() + 3600 * 24).getTime(),
+      new Date().getTime(),
+      new Date().getTime(),
+      req.socket.remoteAddress as string
+    );
+    const token = JsonWebToken.generateToken(JwtPayload);
+    JsonWebToken.verifytoken(
+      token,
+      req.socket.remoteAddress as string,
+      'rrcasaque@hotmail.co'
+    );
+    return res.send('certo');
+  } catch (error) {
     const errors = HandleError.getErrors(error);
     res.status(errors.status).json(errors.message);
   }
