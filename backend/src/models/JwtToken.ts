3073df557008c84b04093a9bee71dd8319b775fa
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-08-12 12:42:26 -0300

    feat: implemented jwt generate token

--- backend/src/models/JwtToken.ts
+++ backend/src/models/JwtToken.ts
@@ -2,9 +2,34 @@ export class JwtToken {
   constructor(
     private issuer: string, //emissor do token
     private subject: string, //id do user a qual o token se refere
-    private expirationTime: Date, //data de expiração do token
-    private notBefore: Date, //data em que o token começa a ser válido
-    private issuedAt: Date, //data em que o token foi emitido
-    private userIP: string
-  ) {}
+    private expirationTime: number, //data de expiração do token
+    private notBefore: number, //data em que o token começa a ser válido
+    private issuedAt: number, //data em que o token foi emitido
+    private userIP: string //endereço de ip do usuário
+  ) {
+    this.issuer = this.issuer;
+    this.subject = this.subject;
+    this.expirationTime = this.expirationTime;
+    this.notBefore = this.notBefore;
+    this.issuedAt = this.issuedAt;
+    this.userIP = this.userIP;
+  }
+  getIssuer() {
+    return this.issuer;
+  }
+  getSubject() {
+    return this.subject;
+  }
+  getExpirationTime() {
+    return this.expirationTime;
+  }
+  getNotBefore() {
+    return this.notBefore;
+  }
+  getIssedAt() {
+    return this.issuedAt;
+  }
+  getUserIP() {
+    return this.userIP;
+  }
 }
--- backend/src/services/JsonWebToken.ts
+++ backend/src/services/JsonWebToken.ts
@@ -1,11 +1,26 @@
 import { JwtToken } from '../models/JwtToken';
 import { IJWT } from './interfaces/IJWT';
+import { sign, JwtPayload } from 'jsonwebtoken';
 
-export class JsonWebToken implements IJWT {
-  generateToken(): string {
-    throw new Error('Method not implemented.');
+class JWT implements IJWT {
+  generateToken(tokenPayload: JwtToken): string {
+    const JwtPayload: JwtPayload = {
+      iss: tokenPayload.getIssuer(),
+      sub: tokenPayload.getSubject(),
+      exp: tokenPayload.getExpirationTime(),
+      nbf: tokenPayload.getNotBefore(),
+      iat: tokenPayload.getIssedAt(),
+      aud: 'api address here',
+      jti: tokenPayload.getUserIP(),
+    };
+    const token = sign(JwtPayload, process.env.JWT_SECRET_KEY as string, {
+      algorithm: 'HS256',
+    });
+    return token;
   }
-  verifytoken(token: JwtToken): boolean {
+  verifytoken(token: string): boolean {
     throw new Error('Method not implemented.');
   }
 }
+
+export const JsonWebToken = new JWT();
