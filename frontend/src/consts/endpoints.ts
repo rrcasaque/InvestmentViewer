a2ce62b3231534a2d11ddff8c8e8ca2a380988ba
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-12-07 01:38:02 -0300

    feat: inserted backend endpoints

--- frontend/src/consts/endpoints.ts
+++ frontend/src/consts/endpoints.ts
@@ -1,8 +1,20 @@
-const API_URL = "";
+const API_URL = "http://localhost:3000";
 
 export const API = {
   AUTH: {
-    REGISTER: API_URL + "",
-    LOGIN: API_URL + "",
+    REGISTER: API_URL + "/register", //POST
+    LOGIN: API_URL + "/login", //POST
+    RECOVERY_CODE: API_URL + "/recoveryCode", //GET /:userEmail
+  },
+  STOCK: {
+    GET_CATEGORIES: API_URL + "/getCategories", //GET protected
+    GET_STOCKS: API_URL + "/", //GET protected /:userId
+    CREATE_STOCKS: API_URL + "/", //POST protected
+    EDIT_STOCKS: API_URL + "/", //PUT protected
+    DELETE_STOCKS: API_URL + "/", //DELETE protected /:stockId
+  },
+  USER: {
+    EDIT_USER: API_URL + "/", //PUT protected
+    RECOVERY_USER_PASSWORD: API_URL + "/recoveryPassword", //PATCH protected
   },
 };
