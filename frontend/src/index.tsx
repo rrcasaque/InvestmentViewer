d209ab2b3eaa25bbf06a88373bc7457d956b7394
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-12-07 23:06:59 -0300

    feat: inserted portal component in index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { ChakraProvider } from '@chakra-ui/react'
-
+import { ChakraProvider } from "@chakra-ui/react";
+import { Modal } from "./components/Modal/Modal";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <ChakraProvider>
+    <Modal />
     <App />
   </ChakraProvider>
 );
