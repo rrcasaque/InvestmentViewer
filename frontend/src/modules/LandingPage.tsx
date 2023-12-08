d8b5602560ec5fc28065c741ec0cf3097515f438
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-12-08 09:48:28 -0300

    feat: added redirect auth page button

--- frontend/src/modules/LandingPage.tsx
+++ frontend/src/modules/LandingPage.tsx
@@ -1,6 +1,9 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 export const LandingPage = () => {
+  const navigate = useNavigate();
+
   return (
     <Flex
       w="full"
@@ -9,8 +12,10 @@ export const LandingPage = () => {
       justify="center"
       bg="black"
       color="white"
+      direction="column"
     >
       landing page
+      <Button onClick={() => navigate("/auth")}>Fazer login</Button>
     </Flex>
   );
 };
