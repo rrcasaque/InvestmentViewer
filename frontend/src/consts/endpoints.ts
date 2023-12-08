8ef631d97a11b25b96034302efd3b09a5ead5fdd
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-12-07 23:05:28 -0300

    feat: included news endpoint address

--- frontend/src/consts/endpoints.ts
+++ frontend/src/consts/endpoints.ts
@@ -2,19 +2,20 @@ const API_URL = "http://localhost:3000";
 
 export const API = {
   AUTH: {
-    REGISTER: API_URL + "/register", //POST
-    LOGIN: API_URL + "/login", //POST
-    RECOVERY_CODE: API_URL + "/recoveryCode", //GET /:userEmail
+    REGISTER: API_URL + "/auth/register", //POST
+    LOGIN: API_URL + "/auth/login", //POST
+    RECOVERY_CODE: API_URL + "/auth/recoveryCode", //GET /:userEmail
+    VALIDATE_TOKEN: API_URL + "/auth/validateToken", //GET
   },
   STOCK: {
-    GET_CATEGORIES: API_URL + "/getCategories", //GET protected
-    GET_STOCKS: API_URL + "/", //GET protected /:userId
-    CREATE_STOCKS: API_URL + "/", //POST protected
-    EDIT_STOCKS: API_URL + "/", //PUT protected
-    DELETE_STOCKS: API_URL + "/", //DELETE protected /:stockId
+    GET_CATEGORIES: API_URL + "/stock/getCategories", //GET protected
+    GET_STOCKS: API_URL + "/stock", //GET protected /:userId
+    CREATE_STOCKS: API_URL + "/stock", //POST protected
+    EDIT_STOCKS: API_URL + "/stock", //PUT protected
+    DELETE_STOCKS: API_URL + "/stock", //DELETE protected /:stockId
   },
   USER: {
-    EDIT_USER: API_URL + "/", //PUT protected
-    RECOVERY_USER_PASSWORD: API_URL + "/recoveryPassword", //PATCH protected
+    EDIT_USER: API_URL + "/user", //PUT protected
+    RECOVERY_USER_PASSWORD: API_URL + "/user/recoveryPassword", //PATCH protected
   },
 };
