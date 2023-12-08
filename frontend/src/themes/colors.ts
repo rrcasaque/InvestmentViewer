e976bd63de9af670c53d4a7b5284801a14534ad4
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-12-07 23:06:41 -0300

    feat: added new colors

--- frontend/src/themes/colors.ts
+++ frontend/src/themes/colors.ts
@@ -1,7 +1,9 @@
 export const COLORS = {
   PRIMARY: "#0e3b65",
-  SECONDARY: "6b7b8b",
-  LIGHT: "f5f5f5",
+  PRIMARY_DARK: "#0e2b46",
+  PRIMARY_LIGHT: "#3f709d",
+  SECONDARY: "#6b7b8b",
+  LIGHT: "#f5f5f5",
   COMPLEMENTARY: "#faf1ca",
   BLACK: "#000000",
   WHITE: "#ffffff",
