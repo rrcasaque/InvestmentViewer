55b5ae438804f073d5b9a63f4f09af01693dbcb9
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2024-02-18 02:18:33 -0300

    chore: changed fileName

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -10,7 +10,7 @@ export const Sidebar = () => {
   const path = usePathname();
 
   return (
-    <div className="flex flex-col items-center justify-around bg-primary-dark w-72 h-screen fixed z-10">
+    <div className="flex flex-col items-center justify-around bg-primary-dark w-72 h-screen fixed z-10 ">
       <Image src={"/lightLogo.png"} alt={""} width={240} height={100} />
       <div className="flex flex-col items-center justify-between h-40 w-full">
         <Link
