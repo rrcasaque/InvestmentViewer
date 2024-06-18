c95378ffc24ac0c9e90c73ee8b539cb18652e08e
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2024-06-18 14:26:29 -0300

    feat: created modal store context

---
+++ frontend/src/contexts/ModalStore.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+export enum EModals {
+  "AddStockModal",
+  "None",
+}
+
+interface IModalState {
+  open: boolean;
+  display: EModals;
+}
+
+const modalState: IModalState = {
+  open: false,
+  display: EModals.None,
+};
+
+export const useModalStore = create(devtools(() => modalState));
+
+export const openModal = (modal: EModals) => {
+  useModalStore.setState({ open: true, display: modal });
+};
+
+export const closeModal = () => useModalStore.setState({ open: false });
